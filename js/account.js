a0415ec98cb38e6d98c786b75c285b949c223798
Sakhatskyi Mark <110189603+Markizdickabraz@users.noreply.github.com>
2024-06-06 23:50:04 +0300

    dasboard scroll, icons

--- js/account.js
+++ js/account.js
@@ -4,6 +4,7 @@ const toggle = body.querySelector('.toggle');
 const searchBtn = body.querySelector('.search_box');
 const modeSwitch = body.querySelector('.toggle_switch');
 const modeText = body.querySelector('.mode_text');
+const logoDashboard = body.querySelector('.dashboard_logo-img');
 
 toggle.addEventListener('click', () => {
     sidebar.classList.toggle('close');
@@ -18,7 +19,9 @@ modeSwitch.addEventListener('click', () => {
 
     if (body.classList.contains('dark')) {
         modeText.innerText = 'Light Mode'
+        logoDashboard.src = './images/sections/svg/logo_white-text.svg';
     } else {
         modeText.innerText = 'Dark Mode'
+        logoDashboard.src = './images/sections/svg/logo_black.svg';
     }
 });
